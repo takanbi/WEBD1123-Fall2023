70c5fa072ef157c8f7a283643cece0ba69387e70
takanbi <tunji.akanbi@century.edu>
2023-09-26 09:19:51 -0500

    finally fixed form issue

--- week4/js/app.js
+++ week4/js/app.js
@@ -1,50 +1,51 @@
+window.addEventListener("DOMContentLoaded", ()=> {
 let button = document.getElementById("button");
 let paragraph = document.getElementById("paragraph");
 // console.log(paragraph);
 const words1 = new Array(
-    "24/7",
-    "multi-Tier",
-    "30,000 foot",
-    "B-to-B",
-    "win-win"
-  );
-  const words2 = new Array(
-    "empowered",
-    "value-added",
-    "oriented",
-    "focused",
-    "aligned"
-  );
-  const words3 = new Array(
-    "process",
-    "solution",
-    "tipping-point",
-    "strategy",
-    "vision"
-  );
+  "24/7",
+  "multi-Tier",
+  "30,000 foot",
+  "B-to-B",
+  "win-win"
+);
+const words2 = new Array(
+  "empowered",
+  "value-added",
+  "oriented",
+  "focused",
+  "aligned"
+);
+const words3 = new Array(
+  "process",
+  "solution",
+  "tipping-point",
+  "strategy",
+  "vision"
+);
 
 // console.log(Math.floor(Math.random() * 10));
 function makePhrase() {
-    // let test = true;
-    let rand1 = Math.floor((Math.random() * words1.length));
-    let rand2 = Math.floor((Math.random() * words2.length));
-    let rand3 = Math.floor((Math.random() * words3.length));
-    let phrase = words1[rand1] + " ," + words2[rand2] + " ," + words3[rand3];
-    // console.log(phrase);
-    paragraph.innerHTML = phrase;
-    // function func() {
-        
-        // if (test) {
-        //     paragraph.style.color = "aquamarine"; 
-        //     test = false;
-        // }
-        // else {
-        //     test = true;
-        //     paragraph.style.color = "red"; 
-        // }
-    //}
-    // paragraph.style.color = "aquamarine";
-} 
+  // let test = true;
+  let rand1 = Math.floor(Math.random() * words1.length);
+  let rand2 = Math.floor(Math.random() * words2.length);
+  let rand3 = Math.floor(Math.random() * words3.length);
+  let phrase = words1[rand1] + " ," + words2[rand2] + " ," + words3[rand3];
+  // console.log(phrase);
+  paragraph.innerHTML = phrase;
+  // function func() {
+
+  // if (test) {
+  //     paragraph.style.color = "aquamarine";
+  //     test = false;
+  // }
+  // else {
+  //     test = true;
+  //     paragraph.style.color = "red";
+  // }
+  //}
+  // paragraph.style.color = "aquamarine";
+}
 
 // const contactCheck = new Object();
 // contactCheck.firstname = "Tunji"
@@ -53,21 +54,31 @@ function makePhrase() {
 const contactCheck = {
   firstname: "Tunji",
   lastname: "Akanbi",
-  
-}
-function getFormInput(e) {
-// function getFormInput() {
-  e.prevent.default();
-  let formContent = document.getElementById("form-content");
+};
+// const form_Content = document.getElementById("form-content");
+// form_Content.classList.add("hidden");
+let formC = document.getElementById("form-content").classList;
+
+function getFormInput() {
+// function getFormInput(e) {
+  // let formContent = document.getElementById("form-content");
+  // e.prevent.default();
+  // let formContent = document.getElementById("form-content");
 
   // let firstName = document.getElementById("firstname");
-  // formContent.textContent = firstName.value;
-  document.getElementById("form-content").style.display = "block";
-//   if (firstName.value == contactCheck.firstname) {
-// alert(" Your firstname is " + firstName.value);
-alert("Ok, it works")
-//   }
+  // form_Content.textContent = firstName.value;
+  // form_Content.classList.remove("hidden");
+  // form_Content.classList.add("show");
 
+  formC.remove("hidden");
+  formC.add("show");
+  console.log("what the f.$%%!!!");
+  //   if (firstName.value == contactCheck.firstname) {
+  // alert(" Your firstname is " + firstName.value);
+  // alert("Ok, it works");
+  //   }
 }
 let button2 = document.getElementById("button2");
-button2.addEventListener("click", getFormInput, false);
+button2.addEventListener("click", getFormInput);
+
+});
